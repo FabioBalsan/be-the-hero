a39e53a38b70bce283852f44e4040928b605fec7
unknown <fabiobalsan@gmail.com>
2020-03-29 18:03:03 -0300

    Validações de rotas e aplicação de testes

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuriton = require('../../knexfile');
 
-const connection = knex(configuriton.development);
+const config = process.env.NODE_ENV == 'test' ? configuriton.test : configuriton.development; 
+
+const connection = knex(config);
 
 module.exports = connection;
